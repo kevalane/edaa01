6e90f9ac2c1763db0910fe0da53132c82df30878
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-01-15 13:45:02 +0100

    Implemented abstract Player class.

---
+++ lab1/src/game/Player.java
@@ -0,0 +1,32 @@
+package game;
+
+public abstract class Player {
+	private String userId;
+	
+	/**
+	 * Constructor that sets private attribute userId according to parameter
+	 * @param userId
+	 * 		string value of userid
+	 */
+	public Player(String userId) {
+		this.userId = userId;
+	}
+	
+	/**
+	 * Getter for private attribute userid
+	 * @return
+	 * 		attribute userid, type string
+	 */
+	public String getUserId() {
+		return this.userId;
+	}
+	
+	/**
+	 * Removes a number of pins from a board
+	 * @param b
+	 * 		the Board object to remove the pins from
+	 * @return
+	 * 		number of pins left after removal
+	 */
+	public abstract int takePins(Board b);
+}
