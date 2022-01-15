7ceabde58dcb310b52a65e4abcf91bbec904a643
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-01-15 18:13:45 +0100

    Fixed game loop for JOptionPane instead.

--- lab1/src/game/TakePinsGame.java
+++ lab1/src/game/TakePinsGame.java
@@ -4,16 +4,15 @@ public class TakePinsGame {
 
 	public static void main(String[] args) {
 		
-		
-		
+		// Game init
 		Board b = new Board();
 		b.setUp(10);
 		boolean playerTurn = true;
 		Player p1 = new HumanPlayer("Kevin");
 		Player p2 = new ComputerPlayer("comp");
 		
+		// While loop for game
 		while(b.getNoPins() > 0) {
-			System.out.println("Det finns " + b.getNoPins() + " pinnar kvar");
 			if (playerTurn) {
 				p1.takePins(b);
 				playerTurn = false;
@@ -25,9 +24,9 @@ public class TakePinsGame {
 		
 		// out, if playerturn, computer won
 		if (playerTurn) {
-			System.out.println("Datorn vann!");
+			UserInterface.printMessage("Datorn vann!");
 		} else {
-			System.out.println("Grattis! Du vann.");
+			UserInterface.printMessage("Grattis! Du vann.");
 		}
 	}
 
