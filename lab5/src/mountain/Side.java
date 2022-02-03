a3d07a6207ac3d30ca81172b48ed185b98eb0a22
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-02-03 18:30:37 +0100

    Implemented side.

---
+++ lab5/src/mountain/Side.java
@@ -0,0 +1,46 @@
+package mountain;
+
+public class Side {
+	
+	private Point p1;
+	private Point p2;
+	private Point mid;
+	
+	/**
+	 * Constructor, sets attrbutes for each parameter
+	 * @param p1, point 1 of this side
+	 * @param p2, point 2 of this side
+	 * @param mid, midpoint of this side
+	 */
+	public Side(Point p1, Point p2, Point mid) {
+		this.p1 = p1;
+		this.p2 = p2;
+		this.mid = mid;
+	}
+	
+	/**
+	 * Getter for p1
+	 * @return Point object of p1
+	 */
+	public Point getP1() {
+		return this.p1;
+	}
+	
+	/**
+	 * Getter for point 2
+	 * @return Point object p2
+	 */
+	public Point getP2() {
+		return this.p2;
+	}
+	
+	/**
+	 * Getter for mid
+	 * @return mid point
+	 */
+	public Point mid() {
+		return this.mid;
+	}
+	
+	
+}
