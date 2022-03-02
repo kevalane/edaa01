11e85b19dfa48668c1bdad9abcde2601d1b73129
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-03-02 20:10:00 +0100

    tested setMatrix()

--- Sudoku/src/sudoku/SudokuSpec.java
+++ Sudoku/src/sudoku/SudokuSpec.java
@@ -1,5 +1,4 @@
 package sudoku;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterEach;
@@ -23,7 +22,7 @@ class SudokuSpec {
 	@Test
 	void testSetMatrix() {
 		// a valid case
-		int[][] m = {{0,0,0,0,0,0,0,0,0},
+		int[][] m1 = {{0,0,0,0,0,0,0,0,0},
 					 {0,0,0,0,0,0,0,0,0},
 					 {0,0,0,0,6,0,0,0,0},
 					 {0,0,0,0,0,0,0,0,0},
@@ -32,13 +31,37 @@ class SudokuSpec {
 					 {0,0,0,0,2,0,0,0,0},
 					 {0,0,0,0,0,0,0,0,0},
 					 {0,0,0,0,0,0,9,0,0}};
-		this.s.setMatrix(m);
+		this.s.setMatrix(m1);
 		
-		assertArrayEquals(m, this.s.getMatrix());
+		assertArrayEquals(m1, this.s.getMatrix());
 		
 		// test wrong dimension
-		int[][] m = {{0,0,1}, {1,1,1}, {5,1,0}};
-		assertThrows(new IllegalArgumentException(), this.s.setMatrix(m));
+		int[][] m2 = {{0,0,1}, {1,1,1}, {5,1,0}};
+		assertThrows(IllegalArgumentException.class, () -> this.s.setMatrix(m2));
+		
+		// test using number larger than 9
+		int[][] m3 = {{0,0,0,0,0,0,0,0,0},
+				 {0,0,0,0,0,0,0,0,0},
+				 {0,0,0,0,6,0,0,0,0},
+				 {0,0,0,14,0,0,0,0,0},
+				 {0,0,0,1,0,0,0,0,0},
+				 {0,0,0,0,0,0,0,0,0},
+				 {0,0,0,0,2,0,0,0,0},
+				 {0,0,0,0,0,0,0,0,0},
+				 {0,0,0,0,0,0,9,0,0}};
+		assertThrows(IllegalArgumentException.class, () -> this.s.setMatrix(m3));
+		
+		// test number less than 0
+				int[][] m4 = {{0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,6,0,0,0,0},
+						 {0,0,0,14,0,0,0,0,0},
+						 {0,0,0,1,0,0,0,0,0},
+						 {0,0,-10,0,0,0,0,0,0},
+						 {0,0,0,0,2,0,0,0,0},
+						 {0,0,0,0,0,0,0,0,0},
+						 {0,0,0,0,0,0,9,0,0}};
+				assertThrows(IllegalArgumentException.class, () -> this.s.setMatrix(m4));
 	}
 
 }
